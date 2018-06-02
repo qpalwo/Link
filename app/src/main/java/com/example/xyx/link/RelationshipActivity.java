c0054d55599ba7cc9dd907906d6fb5ab6be960c5
chen450890252 <1215638092@qq.com>
2018-06-02 16:55:01 +0800

    111

--- app/src/main/java/com/example/xyx/link/RelationshipActivity.java
+++ app/src/main/java/com/example/xyx/link/RelationshipActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by 陈钊燚 on 2018/6/2.
@@ -11,10 +12,11 @@ import android.support.v7.widget.AppCompatImageView;
  * Github FourfireChen
  */
 public class RelationshipActivity extends AppCompatActivity {
-
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relation);
     }
 }
