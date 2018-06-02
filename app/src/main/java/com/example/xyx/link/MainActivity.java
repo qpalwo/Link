dac735513c64ef3fbbd81959640e683ee8a712ec
xyx <qpalwo@qq.com>
2018-06-02 16:39:22 +0800

    bean

--- app/src/main/java/com/example/xyx/link/MainActivity.java
+++ app/src/main/java/com/example/xyx/link/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.xyx.link;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bmob.initialize(this, "e4bdab8ef7e032628a681cf114e5f9fa");
+
     }
+
+   
+
 }
