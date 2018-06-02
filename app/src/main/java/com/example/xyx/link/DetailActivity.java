b41bb5cebf6c0fdac597467d9286416a0ebc183d
xyx <qpalwo@qq.com>
2018-06-02 20:46:50 +0800

    Revert "33333"
    This reverts commit a3cf761

--- app/src/main/java/com/example/xyx/link/DetailActivity.java
+++ app/src/main/java/com/example/xyx/link/DetailActivity.java
@@ -13,6 +13,6 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detail_activity);
+
     }
 }
