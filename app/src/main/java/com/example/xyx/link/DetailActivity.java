63d4e614a1b2da4ae1405be99334f3abb47b12ae
chen450890252 <1215638092@qq.com>
2018-06-02 17:22:53 +0800

    222222

---
+++ app/src/main/java/com/example/xyx/link/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.xyx.link;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 陈钊燚 on 2018/6/2.
+ * QQ 1215638092
+ * Github FourfireChen
+ */
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
