de79c09884f409f0510a290256297d3b2319a7e5
Aoyang Li <liaoyanglay@outlook.com>
2018-06-02 16:40:57 +0800

    add drawer layout

--- app/src/main/java/com/example/xyx/link/MainActivity.java
+++ app/src/main/java/com/example/xyx/link/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.xyx.link;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.avatar_toolbar)
+    RoundedImageView avatarToolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+        }
+        return true;
+    }
+
+    @OnClick(R.id.avatar_toolbar)
+    public void onViewClicked() {
+        drawerLayout.openDrawer(GravityCompat.START);
     }
 }
